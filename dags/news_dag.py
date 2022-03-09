693c997a0f63ea1d379a2ca2262de4e305941f92
osidekyle <hawkinskyle67@gmail.com>
2022-03-09 08:53:31 -0800

    added airflow components

---
+++ dags/news_dag.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner' : 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0,0,0,0,0),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1, 
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1)
+)
