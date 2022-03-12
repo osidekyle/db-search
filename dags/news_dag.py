2d1193d4fcda3d05bbe72d80fb646ad8c89c7e5d
osidekyle <hawkinskyle67@gmail.com>
2022-03-12 00:05:47 -0800

    set up function in dag

--- dags/news_dag.py
+++ dags/news_dag.py
@@ -1,12 +1,14 @@
-"""from datetime import timedelta
+from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
+from news_etl import get_news_data
+
 default_args = {
     'owner' : 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(0,0,0,0,0),
+    'start_date': days_ago(0,1,0,0,0),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -15,10 +17,16 @@ default_args = {
 }
 
 dag = DAG(
-    'spotify_dag',
+    'news_dag',
     default_args=default_args,
     description='Our first DAG with ETL process!',
-    schedule_interval=timedelta(days=1)
+    schedule_interval=timedelta(hours=1)
 )
 
-"""
+run_etl = PythonOperator(
+    task_id = 'Getting_news_data',
+    python_callable=get_news_data,
+    dag=dag
+)
+
+run_etl
